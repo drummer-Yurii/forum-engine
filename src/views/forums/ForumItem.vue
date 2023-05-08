e46428736918667f0dbecf5e15f68902e9809c1e
Yurii Rybachok <drummer2009@ukr.net>
2023-05-08 09:55:51 +0300

    create ForumItem component

---
+++ src/views/forums/ForumItem.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="container">
+        <div class="title">{{ title ?? 'Don\'t have name' }}</div>
+        <div class="description" @click="doStuff">{{ description }}</div>
+        <div v-if="!title">Realy don't have name!!!</div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const title = ref('Main Forum');
+const description = 'Speak here about all!';
+
+function doStuff() {
+    title.value = null;
+    console.log('rename forum');
+    
+}
+</script>
+
+<style scoped>
+.container {
+    margin: 20px 0;
+}
+
+.title {
+    font-size: larger;
+}
+
+.description {
+    font-size: smaller;
+}
+</style>
--- src/views/forums/ForumsView.vue
+++ src/views/forums/ForumsView.vue
@@ -1,29 +1,12 @@
 <template>
     Here be forums list
-
-    <div class="forum_item">
-        <div class="forum_item-title">{{ title }}</div>
-        <div class="forum_item-description">{{ description }}</div>
-    </div>
+    <ForumItem />
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-
-const title = ref('Main Forum');
-const description = 'Speak here about all!';
+import ForumItem from './ForumItem.vue';
 </script>
 
 <style scoped>
-.forum_item {
-    margin: 20px 0;
-}
 
-.forum_item-title {
-    font-size: larger;
-}
-
-.forum_item-description {
-    font-size: smaller;
-}
 </style>
