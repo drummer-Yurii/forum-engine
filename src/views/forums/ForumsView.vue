7c1f34d72ee79d99ce0d011c99a41ecc571f8817
Yurii Rybachok <drummer2009@ukr.net>
2023-05-08 10:28:21 +0300

    send data with v-for

--- src/views/forums/ForumsView.vue
+++ src/views/forums/ForumsView.vue
@@ -1,12 +1,22 @@
 <template>
     Here be forums list
-    <ForumItem :forum="{ id: 'Main', description: 'We speak here about everething'}" />
-    <ForumItem :forum="{ id: 'Mistakes', description: 'Report of bags'}" />
-    <ForumItem :forum="{ id: 'Complaints', description: 'Complaints for moderators'}" />
+
+    <ForumItem 
+        v-for="forum in forums" 
+        :key="forum.id"
+        :forum="forum"
+    />
 </template>
 
 <script setup lang="ts">
+import type { Forum } from '@/api/models/Forum';
 import ForumItem from './ForumItem.vue';
+
+const forums: Forum[] = [
+    { id: 'Main', description: 'We speak here about everething' },
+    { id: 'Mistakes', description: 'Report of bags' },
+    { id: 'Complaints', description: 'Complaints for moderators' }
+]
 </script>
 
 <style scoped>
